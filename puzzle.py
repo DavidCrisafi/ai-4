a147c84a4e407246c369a87fa0a8fc4db4025fbc
Devan Haynes <dhaynes12@alaska.edu>
2019-11-04 14:27:50 -0900

    Added getZoneNum function

--- puzzle.py
+++ puzzle.py
@@ -32,6 +32,65 @@ class Puzzle(object):
                     numList.append(puzzle[colNum][j])
         return numList
 
-
+    # Zone number are 0, 1, 2
+    #                 3, 4, 5
+    #                 6, 7, 8
+    def getZoneNum(self, zoneNum):
+        numList = []
+        if zoneNum == 0:
+            for i in range(0,3):
+                for j in range(0,3):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 1:
+            for i in range(3,6):
+                for j in range(0,3):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 2:
+            for i in range(6,9):
+                for j in range(0,3):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 3:
+            for i in range(0,3):
+                for j in range(3,6):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 4:
+            for i in range(3,6):
+                for j in range(3,6):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 5:
+            for i in range(6,9):
+                for j in range(3,6):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 6:
+            for i in range(0,3):
+                for j in range(6,9):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 7:
+            for i in range(3,6):
+                for j in range(6,9):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        elif zoneNum == 8:
+            for i in range(6,9):
+                for j in range(6,9):
+                    if (puzzle[i][j] != 0):
+                        if puzzle[i][j] not in numList:
+                            numList.append(puzzle[i][j])
+        return numList
 
 
