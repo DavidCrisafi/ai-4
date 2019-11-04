f3b7df1b2f02f0530ad3d4705b5c6324ecdf2682
David Crisafi-Lurtsema <crisafilurtsema@gmail.com>
2019-11-04 14:02:32 -0900

    Printing sudoku

--- sudoku.py
+++ sudoku.py
@@ -17,14 +17,38 @@ def generatePuzzle(f):
         
         line[8] = line[8].strip('\n')
         
-        puzzle.insert(i, line)
+        intLine = []
+        for n in line:
+            intLine.append(int(n))
+        
+        puzzle.insert(i, intLine)
     
     return puzzle
 
 #Beginner function; needs better formatting
 def printPuzzle(puzzle):
-    for p in puzzle:
-        print(p)
+    for i in range(0, 9):
+        line = ""
+        for j in range(0, 9):
+            if (j == 0):
+                line += "| "
+                
+            if (puzzle[i][j] != 0):
+                line += "|" + str(puzzle[i][j])
+            else:
+                line += "|-"
+            if ((j+1) % 3 == 0):
+                line += "| "
+            
+        line += "|"
+        
+        if (i == 0 or i % 3 == 0):
+            if (i != 0):
+                print("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")
+            print("___________________________")
+        print(line)
+    
+    print("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯")
 
 if __name__ == '__main__':
     main()
