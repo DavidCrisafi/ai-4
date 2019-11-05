23cd2d7fb67308067fb99af9edb80b3d5bc59ebd
Jonathan Chronister <jchronister2@alaska.edu>
2019-11-05 14:32:19 -0900

    Documents more Puzzle functions. Modifies the domain initialization function to find the potential values wherever there is a missing value.

--- puzzle.py
+++ puzzle.py
@@ -5,25 +5,61 @@ class Puzzle(object):
         manipulating sudoku boards and returning
         information about them.
     """
-
     def __init__(self, puzzle):
-        """ Sudoku board as a 2D array (9x9 grid) of integers """
+        """ Sudoku board as a 2D array[9][9] (9x9 grid) of integers """
         self.puzzle = puzzle
+        """ Domain is also a 2D array which has the values from the puzzle,
+            but wherever there is a missing value, it inserts a list of
+            potenial values in the missing values position."""
         self.domain = []
 
         self.initDomain()
 
     # initilizing the domain puzzle (0 to a list and number values to set values.
+    """ Initializes the domain list with the actual puzzle values, or if a value is missing
+        it finds a list of potential values."""
     def initDomain(self):
-        for i in range(0,9):
+        for i in range(0, 9):
             newRow = []
             for j in range(0,9):
-                if (self.puzzle[i][j] == 0):
-                    newRow.insert(j, [1,2,3,4,5,6,7,8,9])
+                if self.puzzle[i][j] == 0:
+                    newRow.insert(j, self.getPossibleNumbers(i, j))
+                    # newRow.insert(j, [1,2,3,4,5,6,7,8,9])
                 else:
                     newRow.insert(j, self.puzzle[i][j])
 
             self.domain.insert(i, newRow)
+
+    """ Returns the potential values for a given square 
+        based on its position in the sudoku board.
+        Input the row and column of the sudoku square, 
+        and the function will calculate the rest. """
+    def getPossibleNumbers(self, row, col):
+        """ Figures out which zone its in based on the row and column."""
+        zone = None
+        if row in [0, 1, 2]:
+            zone = [0, 1, 2]
+        elif row in [3, 4, 5]:
+            zone = [3, 4, 5]
+        elif row in [6, 7, 8]:
+            zone = [6, 7, 8]
+
+        if col in [0, 1, 2]:
+            zone = set(zone).intersection([0, 3, 6]).pop()
+        elif col in [3, 4, 5]:
+            zone = set(zone).intersection([1, 4, 7]).pop()
+        elif col in [6, 7, 8]:
+            zone = set(zone).intersection([2, 5, 8]).pop()
+
+        """ Gets the current values from the sudoku board's rows, columns, and grids.
+            Finds the difference between those values and all the potential values
+            for a sudoku board and returns that difference in a list form.
+            ex. The Union of row's, col's, and zone's values unioned returned [1,2,3,4,5].
+                The potential values returned would be [6,7,8,9]."""
+        numList = self.getRowNumbers(row)
+        numList = set(numList).union(self.getColNumbers(col))
+        numList = set(numList).union(self.getZoneNumbers(zone))
+        return set([1, 2, 3, 4, 5, 6, 7, 8, 9]).difference(numList)
 
     """ Gets all the values from a single sudoku board row 
         and returns them as a list """
@@ -43,30 +79,6 @@ class Puzzle(object):
             if (self.puzzle[colNum][j] != 0):
                 if self.puzzle[colNum][j] not in numList:
                     numList.append(self.puzzle[colNum][j])
-        return numList
-
-    def getPossibleNumbers(self, row, col):
-        rowsZones = []
-        colsZones = []
-
-        if row in [0, 1, 2]:
-            rowsZones = [0, 1, 2]
-        elif row in [3, 4, 5]:
-            rowsZones = [3, 4, 5]
-        elif row in [6, 7, 8]:
-            rowsZones = [6, 7, 8]
-
-        if col in [0, 1, 2]:
-            colsZones = [0, 3, 6]
-        elif col in [3, 4, 5]:
-            colsZones = [1, 4, 7]
-        elif col in [6, 7, 8]:
-            colsZones = [2, 5, 8]
-
-        zone = set(rowsZones).intersection(colsZones).pop()
-        numList = self.getRowNumbers(row)
-        numList = set(numList).intersection(self.getColNumbers(col))
-        numList = set(numList).intersection(self.getZoneNumbers(zone))
         return numList
       
     """ Gets all the values for a certain sudoku board grid/zone 
